b91820e5352cc5863176c3adb3879604a42fbb73
Nealena Ras <nealenaras@gmail.com>
2021-10-28 22:57:55 -0400

    minor updates

--- src/components/recipes/RecipeInfo.js
+++ src/components/recipes/RecipeInfo.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Card } from 'react-bootstrap';
+import Checkbox from 'react-checkbox-component';
+import { propTypes } from "react-bootstrap/esm/Image";
+
 
 const RecipeInfo = ({ ingredients }) => {
 
-
-
   return ingredients.map(ingredient => {
     return (
+      <div>
         <div className="form-check" style={{textAlign: 'left'}}>
             <input type="checkbox" className="form-check-label" for="inlineCheckbox1" />
             &nbsp; {ingredient.quantity} {ingredient.measure} {ingredient.food}
         </div>
+      </div>
     );
   });
 };
