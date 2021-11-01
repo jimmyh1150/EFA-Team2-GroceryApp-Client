ab8be69d9e1e341dc7c456c1910677de7af629d8
Nealena Ras <nealenaras@gmail.com>
2021-11-01 16:48:51 -0400

    fixed merge changes

--- src/components/grocerylist/GroceryListDisplay.js
+++ src/components/grocerylist/GroceryListDisplay.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './grocerylist.css';
 
 const DisplayList = (props) => {
     return (
--- src/components/recipes/Recipe.jsx
+++ src/components/recipes/Recipe.jsx
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
 import { Card, Button, Modal, Form, Image } from 'react-bootstrap';
 import './recipe.css';
 import bookmark from './bookmark-1.png';
 import share from './share-2.png';
 import recipe from './link-3.png';
-=======
-import { Card, Button, Modal, Form } from 'react-bootstrap';
-
-
->>>>>>> 38f4bfb849d43cf44be260bbdc82b5561aad5c27
 
 const Recipe = (props) => {
     console.log(props)
     const { label, image, url, totalTime, calories, ingredientLines } = props.recipe;
     const caloriesRounded = calories.toFixed(0);
     const [Item, setItem] = useState([]);
-<<<<<<< HEAD
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -81,80 +74,10 @@ const Recipe = (props) => {
                 </Modal.Body>
             </Modal>
         
-=======
-    const [Ingredient, setIngredient] = useState('')
-
-  
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      var AddItem = event.target.value;
-      console.log(AddItem);
-
-      fetch('http://localhost:3000/grocerylist/create', {
-          method: 'POST',
-          body: JSON.stringify({grocerylist: {item: AddItem}}),
-          headers: new Headers ({
-              'Content-Type': 'application/json',
-              'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjM1NzAzMTYwLCJleHAiOjE2MzU3ODk1NjB9.MmCGY7pMlQeFm2WEMIjxw-OzdJqX7inTndFmsgVoyFA'
-          })
-      }).then( (res) => res.json())
-          .then((Item)=> {
-              console.log(Item);
-              setItem(Item);
-              console.log(Item)
-          })
-    }
-      const [show, setShow] = useState(false);
-      const handleClose = () => setShow(false);
-      const handleShow = () => setShow(true);
-
-    //const filteredItems = prevState.itesm.filter(item => {})
-
-    return (
-      <div>
-              <Card>
-                  <Card.Img src={image} alt={label} className="card-img-top"/>
-                  <Card.Body className="card-body">
-                      <Card.Title>{label}</Card.Title>
-                      <Card.Text>Cooking Time: {totalTime}min <br/>Calories: {caloriesRounded}</Card.Text>
-                      <a href={url} target="_blank" rel="noopener noreferrer">
-                      Go to recipe
-                      </a><br/><br/>
-                      
-                      <Button onClick={handleShow}>View Ingredients</Button>
-
-                      <Modal show={show} onHide={handleClose} size="lg">
-                        <Modal.Header closeButton>
-                            <Modal.Title>{label} <br/>Ingredients <br/> </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                          <Form onSubmit={handleSubmit}>
-
-                            {ingredientLines.map((item) => 
-                            <div>
-                              <input type="checkbox" id={item} name={item} value={item} onClick={handleSubmit} />
-                              <label htmlFor={item}>{item}</label>
-                            </div>)}
-
-                          </Form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button onClick={handleClose}>Close</Button>
-                            <Button type="submit" >Add</Button>
-                        </Modal.Footer>
-                      </Modal>
-                  </Card.Body>
-              </Card>
->>>>>>> 38f4bfb849d43cf44be260bbdc82b5561aad5c27
       </div>  
     );
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 38f4bfb849d43cf44be260bbdc82b5561aad5c27
 export default Recipe;
 
 /*
