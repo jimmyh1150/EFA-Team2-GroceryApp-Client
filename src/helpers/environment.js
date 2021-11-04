dada364d54b6f01649acfaa70fa8ad7e660929e4
jimmyh1150 <89926265+jimmyh1150@users.noreply.github.com>
2021-11-04 19:05:08 -0400

    url updated

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "grocery-app-client.herokuapp.com":
+  case "grocery-app-efa.herokuapp.com":
     APIURL = "https://groceryappserver.herokuapp.com";
 }
 
